6927fadbe5849b599fc97eaf51ae50236708f587
Bruno da Silva Dias <silvadias181@gmail.com>
2016-03-12 12:37:15 -0300

    Exercício 23

--- src/cursojava/exercicios_3/LojaUtilidades.java
+++ src/cursojava/exercicios_3/LojaUtilidades.java
@@ -2,11 +2,17 @@ package cursojava.exercicios_3;
 
 public class LojaUtilidades {
 
-	/**
-	 * @param args
-	 */
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double precoUn = 1.99;
+		
+		System.out.println("Lojas Quase Dois - Tabela de preços");
+		for (int i = 1; i <= 50; i++){
+			
+			System.out.println(i + " - R$ " + (i * precoUn) );
+			
+		}
 
 	}
 
